303d4116bc414a73efb166b25d1a99b8cd7171d8
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2020-08-13 18:36:55 +0600

    A function can be stored in a variable

--- code/firstClassFunction.js
+++ code/firstClassFunction.js
@@ -7,3 +7,19 @@
 * Thanks to - Twinkle Cats 
 
 **************************/
+
+/*** A function can be stored in a variable ***/
+
+function greeting(name){
+    return 'Hello, '+ name;
+}
+var print = greeting("Shihan Chowdhury");
+console.log(print);
+
+// Now we can keep keep greeting function as only a reference in a variable
+var firstVar = greeting;
+console.log(firstVar('Shakib Al Hasan'));
+
+// Again we can use firstVar as a function 
+var secondVar = firstVar;
+console.log(secondVar('Mashrafi Bin Mortaza'));
