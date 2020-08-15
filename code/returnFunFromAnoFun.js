89171d6d03deedd824ccd57b25e3f73fdf54060d
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2020-08-15 09:32:50 +0600

    Return a function from another function

---
+++ code/returnFunFromAnoFun.js
@@ -0,0 +1,83 @@
+/****************************************
+
+* Bismillahir Rahmanir Rahim
+* Functional JavaScript
+* Return a function from another function
+* Author : Shihan Chowdhury
+* Thanks to - Twinkle Cats 
+
+****************************************/
+
+/*************************************************************************
+ * 
+ * A function is a vaule so we can return this value form another function
+ * 
+ ************************************************************************/
+
+
+ function outer() {
+     console.log('I am outer function');
+     function inner() {
+         console.log('I am inner function');
+     }
+     return inner();
+ }
+ outer();
+
+//  OR 
+
+function outer1() {
+    console.log('I am outer function');
+    function inner1() {
+        console.log('I am inner function');
+    }
+    return inner1;
+}
+var result = outer1();
+result();
+
+// same code by using anonymous funciton
+
+function outer2() {
+    console.log('I am outer function');
+    return function() {
+        console.log('I am inner function');
+    }
+}
+var result1 = outer2();
+// result1(); It also can be use.
+console.log(result1());
+
+
+
+/******************************************************/
+
+function outer3(msg) {
+    function inner3(args) {
+        console.log(msg + ', ' + args);
+    }
+    return inner3;
+}
+var res = outer3('Hello');
+res('Shihan');
+var res1 = outer3('How are you');
+res1('Shihan?');
+
+// OR
+
+function outer4(msg) {
+    function inner4(args) {
+        return msg + ', ' + args;
+    }
+    return inner4;
+}
+var x = outer4('Shihan')('Chowhdury');
+x;
+
+// Now with anonymous function
+function outer5(msg) {
+    return function(args) {
+        console.log(msg + ', ' + args);
+    }
+}
+outer5('Shihan')('Chowhdury');
