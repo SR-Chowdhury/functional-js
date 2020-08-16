91ebc83bf2e495d8d527aa5b306de8c7c8340a70
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2020-08-16 08:29:31 +0600

    map function in JS

---
+++ code/mapInJs.js
@@ -0,0 +1,51 @@
+/**************************
+
+* Bismillahir Rahmanir Rahim
+* Functional JavaScript
+* map function in JS
+* Author : Shihan Chowdhury
+* Thanks to - Twinkle Cats 
+
+**************************/
+
+/***************************************************************************
+ * Map Function can take three parameter, 1. element 2. index 3. array
+  
+ **************************************************************************/
+
+ var arr = [2, 4, 6, 8, 10, 12];
+//  We want to add three with each arr element, so lets go
+var newArr = [];
+for(var i = 0; i < arr.length; i++ ) {
+    newArr.push(arr[i] + 2);
+}
+console.log('New array: ' + newArr);
+
+
+// But we are JS programmer we should have minimized the code. So don't waste time bro
+var arr1 = [2, 4, 6, 8, 10, 12];
+var newArr1 = arr1.map(function(element) {
+    return element + 2;
+});
+console.log('New array1: ' + newArr1);
+
+// Now we will create our own map funciton.
+var arr2 = [2, 4, 6, 8, 10, 12];
+var newArr2 = [];
+
+function myMap(arr2, cbf) {
+    for( var i = 0; i < arr2.length; i++) {
+        newArr2.push(cbf(arr2[i]));
+    }
+    return newArr2;
+}
+
+var result = myMap(arr2, function(obj) {
+    return obj + 2;
+});
+result;
+
+
+
+
+
