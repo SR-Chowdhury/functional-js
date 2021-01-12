18bbc43e4b1a558923b78a3f71593d5b1c74e18f
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2021-01-12 18:17:44 +0600

    Output added in closurInJs.js

--- code/closureInJs.js
+++ code/closureInJs.js
@@ -1,7 +1,7 @@
 /****************************************
 
 * Bismillahir Rahmanir Rahim
-* Functional JavaScript
+* Functional JavaScript T-07
 * Closure in JS
 * Author : Shihan Chowdhury
 * Thanks to - Twinkle Cats 
@@ -14,6 +14,7 @@
  * outer funciton's variables or global variables.
  * This approach has doney by Debugger for chrome tools (Extension);
  * Note: when we are going to debugg we must have stop Quokka extension
+ * Besides we can find closure using jsfiddle.net
  * 
  ************************************************************************/
 
@@ -24,19 +25,22 @@ function outer() {
     function inner() {
         var innerVar = 'How are you?';
         console.log(globalVar + ' ' + outVar + ' ' + innerVar);
+        // hello Shihan How are you?
     }
     return inner();
 }
+// console.dir(outer);
 outer();
+
 
 // Example 2 with anonymous function 
 
 function greeting(args) {
     return function(val) {
-        console.log( args + ' ' + val);
+        console.log( args + ' ' + val); // Mr. Shihan
     }
 }
-var x = greeting('two');
-// Above this x has no closure
-x('three');
-// but this x has a closure.
+var x = greeting('Mr.');
+// Above this x has no closure; but args = Mr. is Gloval for greeting-> anonymous function
+x('Shihan');
+// but this x has a closure (Mr.)
