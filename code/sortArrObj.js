4ed5fbe239289e90c24def5a9865901c49d9be2e
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2021-01-12 19:33:29 +0600

    Output added in sortArrObj.js

--- code/sortArrObj.js
+++ code/sortArrObj.js
@@ -1,7 +1,7 @@
 /**************************
 
 * Bismillahir Rahmanir Rahim
-* Functional JavaScript
+* Functional JavaScript T-08
 * Sort Arry of Object
 * Author : Shihan Chowdhury
 * Thanks to - Twinkle Cats 
@@ -15,13 +15,19 @@
 
 var arr = [10, 50, 20, 60, 5, 45, 80, 2];
 arr.sort();
-arr;
-// arr.sort();  Result = 10, 2, 45, 5, 50, 60, 80 [Not Ok] So,
+console.log(arr);
+// arr.sort();  Result = 10, 2, 20, 45, 5, 50, 60, 80 [Not Ok] So,
 arr.sort(function (a, b) {
-    return a-b;
+    return a - b;
 });
-arr;
+console.log(arr); // 2, 5, 10, 20, 45, 50, 60, 80
 // Horrah! we made this in Ascending Form, but we want to Desencending...
+
+arr.sort(function (a, b) {
+    return b - a;
+});
+console.log(arr); // 80, 60, 50, 45, 20, 10, 5, 2
+
 
 // Another Example
 
@@ -32,17 +38,19 @@ function sorting(arr1, ascending, descending) {
     descending(arr1);
 }
 sorting(arr1, function (args) {
-        console.log(arr1);
+        // console.log(arr1);
         args.sort(function (a, b) {
             return a - b;
         });
         console.log('Ascending order of arr1  : ' + args);
+        // Ascending order of arr1  :  2, 5, 10, 20, 45, 50, 60, 80
     },
     function (args) {
         args.sort(function (a, b) {
             return b - a;
         });
         console.log('Descending order of arr1  : ' + args);
+        // Descending order of arr1 : 80, 60, 50, 45, 20, 10, 5, 2
     }
 );
 
@@ -65,18 +73,22 @@ function sortingArrObj(obj, cbf1, cbf2, cbf3, cbf4) {
     cbf3(obj);
     cbf4(obj);
 }
-sortingArrObj(people, function(obj) {
-    obj.sort(function(a, b) {
-        return a.age - b.age;
-    });
-    console.log('Ascending Order of poeple array: ' + obj);
-},
+sortingArrObj(people,
+    
+    function(obj) {
+        obj.sort(function(a, b) {
+            return a.age - b.age;
+        });
+        console.log(obj);
+    },
+
     function(obj) {
         obj.sort(function(a, b) {
             return b.age - a.age;
         });
-        console.log('Descending Order of poeple array: ' + obj);
+        console.log(obj);
     },
+    
     // Now soring by object name (Ascending Order)
     function(obj) {
         obj.sort(function(a, b) {
